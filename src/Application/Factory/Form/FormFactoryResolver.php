1370d50d7f2f2c1508de74b23971c16d38291abc
Anackor <ericbercal@hotmail.com>
2025-04-10 19:19:57 +0200

    Add usefull comments on FormFactoryResolver

--- src/Application/Factory/Form/FormFactoryResolver.php
+++ src/Application/Factory/Form/FormFactoryResolver.php
@@ -5,6 +5,22 @@ namespace App\Application\Factory\Form;
 use App\Domain\Form\FormFactoryInterface;
 use InvalidArgumentException;
 
+/**
+ * The FormFactoryResolver is responsible for resolving and returning the appropriate form factory 
+ * based on the requested type. This is a key part of the Abstract Factory pattern, where the goal 
+ * is to provide a way to create families of related objects without specifying their concrete 
+ * classes. In this case, it resolves which specific form factory (Html, Slack, Console) should 
+ * be used based on the type provided.
+ *
+ * The Abstract Factory pattern is useful when there are multiple families of products (in this 
+ * case, form components such as TextField, Button, and Checkbox) that need to be created for 
+ * different environments (HTML, Slack, Console). By using this pattern, the system is flexible 
+ * and easily extensible to support new types of form factories in the future without modifying 
+ * the core logic of the application.
+ *
+ * The FormFactoryResolver acts as the central point of access to the form factories, making it 
+ * easy to resolve the correct factory to generate platform-specific forms and components.
+ */
 class FormFactoryResolver
 {
     public function __construct(
