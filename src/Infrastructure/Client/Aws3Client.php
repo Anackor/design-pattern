1ea2b2124287fbd187724bc7c14876ad6a2c347d
Anackor <ericbercal@hotmail.com>
2025-05-07 22:28:04 +0200

    Client Interfaces and classes + services using Factory

---
+++ src/Infrastructure/Client/Aws3Client.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Infrastructure\Client;
+
+use App\Domain\Client\AwsS3ClientInterface;
+use Aws\S3\S3Client;
+use Aws\Result;
+
+class AwsS3Client implements AwsS3ClientInterface
+{
+    private S3Client $client;
+
+    public function __construct(private string $bucket, string $key, string $secret, string $region)
+    {
+        $this->client = new S3Client([
+            'region' => $region,
+            'version' => 'latest',
+            'credentials' => [
+                'key' => $key,
+                'secret' => $secret,
+            ],
+        ]);
+    }
+
+    public function putObject(array $args): void
+    {
+        $this->client->putObject($args);
+    }
+
+    public function getObject(array $args): Result
+    {
+        return $this->client->getObject($args);
+    }
+
+    public function deleteObject(array $args): void
+    {
+        $this->client->deleteObject($args);
+    }
+}
